10f77cc055f8c9d36b23289bcfe8e24979bb052a
phoziswa <phoziswanethe14@gmail.com>
2019-11-05 11:29:34 +0200

    added travis config

--- test/greetings.test.js
+++ test/greetings.test.js
@@ -3,7 +3,7 @@ const GreetingFactory = require('../greetings');
 const pg = require("pg");
 const Pool = pg.Pool;
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://codex:codex123@localhost:5432/names_greeted';
+const connectionString = process.env.DATABASE_URL || 'postgresql://codex:codex123@localhost:5432/names_greeted_test';
 
 const pool = new Pool({
   connectionString
